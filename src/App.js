7ee627278c178a4a75f393927ce481c16e8c88af
Renato <renato.tan@blackinktech.io>
2024-02-08 10:22:05 +0800

    More changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import CustomButton from "./CustomButton";
+import { useState } from "react";
 
 function App() {
+  const [confirmText, setConfirmText] = useState("");
+  const [yesPressed, setYesPressed] = useState(false);
+  const [addedText, setAddedText] = useState({
+    very: "",
+    sure: "",
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexWrap: "wrap",
+        flexDirection: "column",
+        alignContent: "center",
+      }}
+    >
+      <p
+        style={{
+          padding: "0",
+          margin: "0",
+          textAlign: "center",
+          alignSelf: "center",
+          fontSize: "4rem",
+        }}
+      >
+        WILL YOU BE MY VALENTINE?
+      </p>
+      <div style={{ width: "25%", alignSelf: "center" }}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+          className="filter-red"
         >
-          Learn React
-        </a>
-      </header>
+          <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
+        </svg>
+        <p>{confirmText}</p>
+        {!yesPressed ? (
+          <div className="centered-style">
+            <CustomButton
+              buttonText="Yes"
+              onClick={() => {
+                setConfirmText("yay!");
+                setYesPressed(true);
+              }}
+            />
+            <p
+              style={{
+                padding: "0",
+                margin: "0",
+                textAlign: "center",
+                alignSelf: "center",
+              }}
+            >
+              or
+            </p>
+            <CustomButton
+              buttonText="No"
+              onClick={() => {
+                if (Math.random() < 0.5) {
+                  setAddedText({
+                    very: addedText.very + " very",
+                    sure: addedText.sure,
+                  });
+                } else {
+                  setAddedText({
+                    very: addedText.very,
+                    sure: addedText.sure + " sure",
+                  });
+                }
+                if (!confirmText) setConfirmText("Are you sure?");
+                else
+                  setConfirmText(
+                    `Are you${addedText.very} sure${addedText.sure}?`
+                  );
+              }}
+            />
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
